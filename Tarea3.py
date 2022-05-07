5915f461b72636a46519e682b6bb1d1df8950c00
carlosjaravas <carlosjaravas@gmail.com>
2022-05-06 18:39:26 -0600

    .py funcional y casi listo para entrega

--- Tarea3.py
+++ Tarea3.py
@@ -8,10 +8,8 @@ from skimage.draw import circle_perimeter
 from sklearn.cluster import KMeans
 from sklearn.utils import shuffle
 from skimage.color import rgb2gray
-import matplotlib.pyplot as plt
-from skimage.draw import circle_perimeter
 
-
+#-*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*
 
 #PARAMETROS
 #Tamaño de trabajo
@@ -23,8 +21,9 @@ N_COLORS = 2
 MAX_RHO = 18
 MAX_THETA = 5*np.pi/180
 
-#Obtener la direccion de las imagenes
-path = os.getcwd()
+#-*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*
+
+#FUNCIONES
 
 def edit_image(image):
     y,x = image.shape[:2]
@@ -41,12 +40,10 @@ def recreate_image(codebook, labels, w, h):
 def img_to_2_colors(img):
     # Copia de la imagen por cuantificar
     img_5_colors = img
-
     # Convert to floats instead of the default 8 bits integer coding. Dividing by
     # 255 is important so that plt.imshow behaves works well on float data (need to
     # be in the range [0-1])
     img_5_colors = np.array(img_5_colors, dtype=np.float64) / 255
-
     # Load Image and transform to a 2D numpy array.
     w, h, d = original_shape = tuple(img_5_colors.shape)
     assert d == 3
@@ -120,4 +117,39 @@ def segmentacion_por_dado(dados,puntos_unicos):
     puntos_d1 = dado_correspondiente.count(0)
     puntos_d2 = dado_correspondiente.count(1)
     puntos_d3 = dado_correspondiente.count(2)
-    return [puntos_d1,puntos_d2,puntos_d3]
+    total = puntos_d1 + puntos_d2 + puntos_d3
+    return [puntos_d1,puntos_d2,puntos_d3],total
+
+def cuenta_puntos(image):
+    img_edit = edit_image(image)
+    COLORES, img_quant=img_to_2_colors(img_edit)
+    image_gray = rgb2gray(img_quant)*255**3/2
+    edgesHT = canny(image_gray, 2, 1, 25)
+    linesHT1 = probabilistic_hough_line(edgesHT, threshold=10, line_length=30, line_gap=3)
+    linesHT2 = delete_long(linesHT1)
+    puntosHT = lineas2puntos(linesHT2)
+    centroides = centroides_dados(puntosHT)
+    c_puntos_dados = image_circles(edgesHT)
+    puntos_unicos = uniq_circles(c_puntos_dados)
+    puntos_por_dado,total = segmentacion_por_dado(centroides,puntos_unicos)
+    return puntos_por_dado,total
+
+def main():
+    if os.path.exists("report_file.txt"):
+        os.remove("report_file.txt")
+    report_file = open("report_file.txt", "x")
+    path = os.getcwd()
+    folder = 'Imagenes_por_analizar'
+    folder_path = os.path.join(path, folder)
+    files = os.listdir(folder_path)
+    for file in files:
+        res_report = ''
+        file_path = os.path.join(folder_path, file)
+        foto = io.imread(file_path)
+        puntos_por_dado, puntos_totales = cuenta_puntos(foto)
+        resultado = f": Valor de cada dado: {puntos_por_dado[0]}, {puntos_por_dado[1]} y {puntos_por_dado[2]}, y los puntos totales son {puntos_totales}."
+        res_report =file + resultado + '\n'
+        report_file.write(res_report)
+    report_file.close()
+
+if __name__ == "__main__": main()
